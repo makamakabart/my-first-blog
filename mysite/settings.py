bf2ef9390d32815b5ce3fb444cd9b767cf36cc56
bartek <bartek@bartek.de>
2018-04-05 22:46:33 +0200

    drugi

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'i9kdny^npg1t$p8ypu4a3*onx08e9rmp)2n@6ce2@wyz2t0wd6'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
@@ -119,3 +119,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
